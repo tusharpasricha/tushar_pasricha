d9fed6af660e453948d247c7920437c2e34e1ebb
tusharpasricha <tusharpasricha21@gmail.com>
2023-01-08 12:03:16 +0530

    simple animation

--- src/components/main/about.js
+++ src/components/main/about.js
@@ -1,22 +1,26 @@
 import React from "react";
 import classes from "./about.module.css";
+import {bounce} from 'react-animations'
+import styled,{keyframes} from 'styled-components'
 
 import { FaCaretDown } from "react-icons/fa";
 
 import { useState } from "react";
 
 const About = () => {
+  const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} `;
+
   const [mainintro, setmainintro] = useState("Hi,I am Tushar Pasricha");
   const [about, setabout] = useState("");
   function showabout() {
     setmainintro("Hi");
     setabout(
-      "I am a Computer Science Undergrad at JIIT noida, currently in 3rd year."
+      "I am a pre final year Computer Science Undergrad at JIIT noida."
     );
   }
   return (
     <div className={classes.main}>
-      <div className={classes.mainintro}>{mainintro}</div>
+      <div className={classes.mainintro}><Bounce>{mainintro}</Bounce></div>
 
       <button className={classes.aboutbtn} onClick={showabout}>
         {" "}
