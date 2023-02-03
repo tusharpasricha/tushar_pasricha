10f990ee46d9eaf90be1b53a5e50590d8d755e90
tusharpasricha <tusharpasricha21@gmail.com>
2023-02-03 23:51:02 +0530

    about btn

--- src/components/main/about.js
+++ src/components/main/about.js
@@ -3,7 +3,7 @@ import classes from "./about.module.css";
 import {bounce} from 'react-animations'
 import styled,{keyframes} from 'styled-components'
 
-import { FaCaretDown } from "react-icons/fa";
+import { FaCaretDown , FaCaretUp} from "react-icons/fa";
 
 import { useState } from "react";
 
@@ -12,19 +12,37 @@ const About = () => {
 
   const [mainintro, setmainintro] = useState("Hi,I am Tushar Pasricha");
   const [about, setabout] = useState("");
+  const [c,setc]=useState(0);
+  
   function showabout() {
-    setmainintro("Hi");
-    setabout(
-      "I am a pre final year Computer Science Undergrad at Jaypee Institute of Information Technology Noida, India. I am a Blockchain enthusiast and am also into Web development "
-    );
+    if(mainintro==="Hi")
+    {
+      setc(0);
+      setmainintro("Hi,I am Tushar Pasricha")
+      setabout("");
+      
+    }
+    else{
+      setc(1);
+      setmainintro("Hi");
+      setabout(
+        "I am a pre final year Computer Science Undergrad at Jaypee Institute of Information Technology Noida, India. I am a Blockchain enthusiast and am also into Web development. "
+      );
+    }
+   
   }
   return (
     <div className={classes.main}>
       <div className={classes.mainintro}><Bounce>{mainintro}</Bounce></div>
 
       <button className={classes.aboutbtn} onClick={showabout}>
-        {" "}
-        <FaCaretDown />{" "}
+        {c===0 &&<FaCaretDown />}
+        {c===1&& <FaCaretUp />}
+
+        
+        
+        
+        
       </button>
 
       <div className={classes.aboutdata}>{about}</div>
