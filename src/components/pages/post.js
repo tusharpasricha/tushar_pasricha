7b1ab9c02f2c32ada6e38c2b8177f33d6ff6deed
tusharpasricha <tusharpasricha21@gmail.com>
2023-06-25 14:55:55 +0530

    resume aadded

--- src/components/pages/post.js
+++ src/components/pages/post.js
@@ -3,192 +3,46 @@ import classes from './post.module.css'
 import Footer from '../main/footer';
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { Object } from '../main/object';
 
 
 const Post = () => {
-  return (
-    <div className={classes.postpage}>
+    return (
+        <div className={classes.postpage}>
 
-    <div className={classes.back}>
-      <Link className={classes.posta} to="/">
-      <FaArrowLeft className={classes.faback}/>
-       Home
-       </Link>
-    </div>
-
-    <div className={classes.post}>
-      
-
-      <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
+            <div className={classes.back}>
+                <Link className={classes.posta} to="/">
+                    <FaArrowLeft className={classes.faback} />
+                    Home
+                </Link>
             </div>
-            <div className={classes.text}>
-                <div>
-                 <text className={classes.textorange}>
-                 <br></br>
-                 <h1> Things I learn helping my father in business</h1>
-                
-                 <br></br>
-                 <br></br>
-                 My father runs a wholesale shop and I used to help him before entering college and sometimes now also when I go home from my hostel. 
-I am so proud of how he grew his business.
-There are a lot of things that I learn there.
-
-<br></br>
-<br></br>
-<h3>PARTNERSHIP</h3>
-
-Papa started his business in 2008 with his friend as a business partner.
-I call him Chachu.
-From sharing the workload to sharing the cost and the profit they managed it together. Disagreements can occur in partnership and It's important to handle them.
-It's been years and still they both are more than brothers and his family and our family share good relations.
-<br></br>
-<br></br>
-<h3>CUSTOMER RELATIONSHIPS & CUSTOMER LOYALTY</h3> 
-
-Customer relationships are built on trust and Trust only happens in stages, gaining trust takes work.
-There’s an uncle who comes twice every day with a bag on his cycle carrier and buys goods to sell in his shop.  He is unschooled and faces problems in calculations and in counting money. I remember sometimes he even gives all the money he has and says “aapke jitne bante h lelo”. 
-<br></br>
-<br></br>
-
-<h3>PEOPLE MANAGEMENT</h3>
-
-You come across a lot of people with different minds in the shop. It's a great place to be able to exchange energy with people.
-There are also challenging circumstances in which customers are difficult to handle.
-<br></br>
-<br></br>
-<h3>ATTENTION AND FOCUS</h3>
-
-You can’t zoned out at the shop
-I remember a few times I screwed up in counting back change.
-You have to keep your eyes everywhere. 
-I remember an incident when a customer put an item in his bag without paying
-<br></br> 
-                </text> 
+            <div className={classes.post}> 
+            <a href="https://www.linkedin.com/pulse/5-valuable-lessons-i-learned-helping-my-father-his-tushar-pasricha/" target="_blank"> 
+            <div className={classes.card}>
+                <div className={classes.maincontent}>
+                    <div className={classes.header}>
+                        <span>Article on</span>
+                        <span>6-April-2023</span>
+                    </div>
+                    <p className={classes.heading}>5 Valuable Lessons I Learned Helping My Father in His Business</p>
+                    <div className={classes.categories}>
+                        <span>Article</span>
+                        {/* <span>Css</span> */}
+                    </div>
                 </div>
-               
-            </div>
-    </div>
-    {/* <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
+                <div className={classes.footer}>
+                    by Tushar Pasricha
                 </div>
-
             </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>How much a dollar cost?</text> 
-                </div>
-               
+            </a>
             </div>
-    </div>
-    <div className={classes.postsafari}>
-            <div className={classes.nav}>
+            <Object/>
 
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
-            </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>Be careful with who sits in your head</text> 
-                </div>
-               
-            </div>
-    </div>
-    <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
-            </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>Keep a disagreement away from becoming an argument</text> 
-                </div>
-               
-            </div>
-    </div>
-
-    <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
-            </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>Excitement automatically makes you punctual.</text> 
-                </div>
-               
-            </div>
-    </div>
-
-
-    <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
-            </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>How lazy are you? I greet people by raising my eyebrows.</text> 
-                </div>
-               
-            </div>
-    </div>
-
-    <div className={classes.postsafari}>
-            <div className={classes.nav}>
-
-                <div className={classes.circle}>
-                    <div class={classes.circle1}></div>
-                    <div class={classes.circle2}></div>
-                    <div class={classes.circle3}></div>
-                </div>
-
-            </div>
-            <div className={classes.text}>
-                <div>
-                <text className={classes.textorange}>The only failure in 20s is inactivity. The rest is trail and error.</text> 
-                </div>
-               
-            </div>
-    </div> */}
-
-
-
-    </div>
-    <Footer/>
-    </div>
-  )
+            <br></br>
+            <br></br>
+            <Footer />
+        </div>
+    )
 }
 
 export default Post
--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import "./index.css";
 import App from "./App";
 import Post from "./components/pages/post";
 import Project from "./components/pages/project";
+import SocialBar from "./components/main/socialbar";
 
 ReactDOM.render(
+  
+  
   <BrowserRouter>
+  
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/project" element={<Project />} />
@@ -15,4 +19,5 @@ ReactDOM.render(
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
+  
 );
