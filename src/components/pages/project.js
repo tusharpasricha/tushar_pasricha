796b204a2d5cf0fdcb2abeef32cca5941883f94e
tusharpasricha <tusharpasricha21@gmail.com>
2023-06-25 19:13:01 +0530

    add tags

--- src/components/pages/project.js
+++ src/components/pages/project.js
@@ -20,81 +20,81 @@ import { useLocation } from 'react-router-dom';
 const scrolldata = [
   {
     heading: "Freelancing website using Blockchain",
-    img:freelancing,
+    img: freelancing,
     link: "https://github.com/tusharpasricha/Dapp-freelancing",
-    glink:"https://github.com/tusharpasricha/Dapp-freelancing",
-    desc: "Freelancing application used to create a contract between freelancer and employer increasing the trustability. Used Solidity to design Ethereum Smart Contract.",
+    glink: "https://github.com/tusharpasricha/Dapp-freelancing",
+    desc: "Freelancing application used to create a contract between freelancer and employer increasing the trustability. ",
   },
   {
     heading: "Rent Tracker",
-    img:renttraker,
+    img: renttraker,
     link: "https://renttracker.vercel.app/",
-    glink:"https://github.com/tusharpasricha/renttracker",
+    glink: "https://github.com/tusharpasricha/renttracker",
     desc: "Rent Tracking website where you can manage your rented property.",
   },
   {
     heading: "Bitcoin Explorer",
     img: bitcoin,
     link: "https://github.com/tusharpasricha/Bitcoin-Explorer",
-    glink:"https://github.com/tusharpasricha/Bitcoin-Explorer",
+    glink: "https://github.com/tusharpasricha/Bitcoin-Explorer",
     desc: "A bitcoin explorer with a feature where you can interact with like minded bitcoiners",
   },
   {
     heading: "Charity Dapp",
-    img:charity,
+    img: charity,
     link: "https://github.com/tusharpasricha/Dapp-charity",
-    glink:"https://github.com/tusharpasricha/Dapp-charity",
+    glink: "https://github.com/tusharpasricha/Dapp-charity",
     desc: "Decentralized application to restore trust in charity and make sure the charity reach direct to the intended recipients through Smart Contract using Solidity.",
   },
   {
     heading: "Sir Johnson Socks",
-    img:ss,
+    img: ss,
     link: "https://www.sirjohnsonsocks.com/",
-    glink:"https://github.com/tusharpasricha/sirjohnsonsocks",
+    glink: "https://github.com/tusharpasricha/sirjohnsonsocks",
     desc: "Website for a socks brand made using Reactjs and hosted on versel",
   },
   {
     heading: "TabX",
     img: tabx,
-    link:"https://github.com/tusharpasricha/tabX",
-    glink:"https://github.com/tusharpasricha/tabX",
+    link: "https://github.com/tusharpasricha/tabX",
+    glink: "https://github.com/tusharpasricha/tabX",
     desc: "Extension to share URL of your current tab straight away with a click. Its a group project for my first Hackathon",
   },
-  
+
   {
     heading: "Don't Stop",
     img: dont,
-    link:"https://dontstop-xi.vercel.app/",
-    glink:"https://github.com/tusharpasricha/Don-tStop",
+    link: "https://dontstop-xi.vercel.app/",
+    glink: "https://github.com/tusharpasricha/Don-tStop",
     desc: "A travel journal app to write your everyday travel journal",
   },
   {
     heading: "Every Penny Cost",
     img: everypenny,
-    link:"https://everypennycounts.vercel.app/",
-    glink:"https://github.com/tusharpasricha/everypennycounts",
+    link: "https://everypennycounts.vercel.app/",
+    glink: "https://github.com/tusharpasricha/everypennycounts",
     desc: "React App for tracking expenses",
   },
   {
     heading: "Owlsome Coffee",
     img: owlsome,
-    link:"",
-    glink:"https://github.com/tusharpasricha/owlsomecoffee",
+    link: "",
+    glink: "https://github.com/tusharpasricha/owlsomecoffee",
     desc: "Website for a coffee brand",
   },
 
   {
     heading: "Elliptical",
     img: elliptical,
-    link:"https://elliptical.herokuapp.com/html/home.html",
-    glink:"https://github.com/sarthakkathpalia/elliptical",
+    link: "https://elliptical.herokuapp.com/html/home.html",
+    glink: "https://github.com/sarthakkathpalia/elliptical",
     desc: "A group project for my first hackathon",
   },
   {
     heading: "TabX",
     img: tabx,
-    link:"https://github.com/tusharpasricha/tabX",
-    glink:"https://github.com/tusharpasricha/tabX",
+    link: "https://github.com/tusharpasricha/tabX",
+    glink: "https://github.com/tusharpasricha/tabX",
     desc: "Extension to share URL of your current tab straight away with a click. Its a group project for my first Hackathon",
   },
 ];
@@ -102,10 +102,11 @@ const scrolldata = [
 const Project = () => {
   const location = useLocation();
 
-// Scroll to the top of the page whenever the location changes
-useEffect(() => {
-  window.scrollTo(0, 0);
-}, [location]);
+  // Scroll to the top of the page whenever the location changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className={classes.projectpage}>
       <div className={classes.back}>
@@ -120,12 +121,17 @@ useEffect(() => {
           <div className={classes.img}>
             <img src={scrolldata[0].img} alt="" />
           </div>
-
           <div className={classes.part2}>
             <div className={classes.data}>
               <h3>{scrolldata[0].heading}</h3>
               <p>{scrolldata[0].desc}</p>
+              <div className={classes.categories}>
+              <span>Solidity</span>
+              <span>Metamask</span>
+              <span>React</span>
+              </div>
             </div>
+            
 
             <div className={classes.options}>
               <a
@@ -152,6 +158,10 @@ useEffect(() => {
             <div className={classes.data}>
               <h3>{scrolldata[1].heading}</h3>
               <p>{scrolldata[1].desc}</p>
+              <div className={classes.categories}>
+              <span>React</span>
+              <span>Firebase</span>
+              </div>
             </div>
             <div className={classes.options}>
               <a target="_blank" className={classes.opt} href={scrolldata[1].link}>
@@ -174,9 +184,14 @@ useEffect(() => {
             <div className={classes.data}>
               <h3>{scrolldata[2].heading}</h3>
               <p>{scrolldata[2].desc}</p>
+              <div className={classes.categories}>
+              <span>React</span>
+              <span>Node</span>
+              <span>Api</span>
+              </div>
             </div>
             <div className={classes.options}>
-              <a target="_blank"className={classes.opt} href={scrolldata[2].link}>
+              <a target="_blank" className={classes.opt} href={scrolldata[2].link}>
                 {" "}
                 Demo{" "}
               </a>
@@ -196,13 +211,18 @@ useEffect(() => {
             <div className={classes.data}>
               <h3>{scrolldata[3].heading}</h3>
               <p>{scrolldata[3].desc}</p>
+              <div className={classes.categories}>
+              <span>Solidity</span>
+              <span>Metamask</span>
+              <span>React</span>
+              </div>
             </div>
             <div className={classes.options}>
-              <a target="_blank"className={classes.opt} href={scrolldata[0].link}>
+              <a target="_blank" className={classes.opt} href={scrolldata[0].link}>
                 {" "}
                 Demo{" "}
               </a>
-              <a target="_blank"className={classes.opt} href={scrolldata[3].glink}>
+              <a target="_blank" className={classes.opt} href={scrolldata[3].glink}>
                 {" "}
                 Code
               </a>
@@ -218,13 +238,16 @@ useEffect(() => {
             <div className={classes.data}>
               <h3>{scrolldata[4].heading}</h3>
               <p>{scrolldata[4].desc}</p>
+              <div className={classes.categories}>
+              <span>React</span>
+              </div>
             </div>
             <div className={classes.options}>
-              <a target="_blank"className={classes.opt} href={scrolldata[4].link}>
+              <a target="_blank" className={classes.opt} href={scrolldata[4].link}>
                 {" "}
                 Demo{" "}
               </a>
-              <a target="_blank"className={classes.opt} href={scrolldata[4].glink}>
+              <a target="_blank" className={classes.opt} href={scrolldata[4].glink}>
                 {" "}
                 Code
               </a>
@@ -241,13 +264,16 @@ useEffect(() => {
             <div className={classes.data}>
               <h3>{scrolldata[5].heading}</h3>
               <p>{scrolldata[5].desc}</p>
+              <div className={classes.categories}>
+              <span>javascript</span>
+              </div>
             </div>
             <div className={classes.options}>
-              <a target="_blank"className={classes.opt} href={scrolldata[5].link}>
+              <a target="_blank" className={classes.opt} href={scrolldata[5].link}>
                 {" "}
                 Demo{" "}
               </a>
-              <a target="_blank"className={classes.opt} href={scrolldata[5].link}>
+              <a target="_blank" className={classes.opt} href={scrolldata[5].link}>
                 {" "}
                 Code
               </a>
