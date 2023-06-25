540578d77489027dcaf2f45e03cc5e145a9e9d75
tusharpasricha <tusharpasricha21@gmail.com>
2023-06-25 14:09:35 +0530

    design changes

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,14 +4,19 @@ import Bar from "./bar";
 import Footer from "./footer";
 import Code from "./code";
 import Skills from "./skills";
+import { Socialbar2 } from "./socialbar2";
+import { Object } from "./object";
 
 function Main() {
   return (
     <>
+      
       <SocialBar />
       <Code/>
       <About />
       <Bar />
+      <Socialbar2/>
+      <Object/>
       <Skills/>
       <Footer />
     </>
---
+++ src/components/main/object.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./object.css"
+
+export const Object = () => {
+  return (
+    <div className='object'>
+    <div class="bg-black py-5 d-flex justify-content-center align-items-center">
+    <div class="obj">
+        <div class="objchild">
+            <span class="inn6"></span>
+        </div>
+    </div>
+    </div>
+    </div>
+  )
+}
