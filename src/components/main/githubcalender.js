0c0fdc9ad59e01695a425234375d1ea16d03a949
tusharpasricha <tusharpasricha21@gmail.com>
2023-07-25 22:35:07 +0530

    add link to github calendar

--- src/components/main/githubcalender.js
+++ src/components/main/githubcalender.js
@@ -3,14 +3,24 @@ import GitHubCalendar from "react-github-calendar";
 import './githubcalender.css'
 const GitHubContributionsChart = () => {
   return (
+    
     <div className="calendar">
+      <a href='https://github.com/tusharpasricha' target="_blank" >
+      <div className="cal">
+
       <h2>GitHub</h2>
       <GitHubCalendar
         colorScheme="dark"
         username="tusharpasricha"
         color="green" // You can choose from 'green', 'blue', or 'pink'
       />
+      
+      </div>
+      </a>
     </div>
+
+
+
   );
 };
 
