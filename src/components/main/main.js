88b28277b9c78c23bfb51035a2ad9c3be145aaed
tusharpasricha <tusharpasricha21@gmail.com>
2026-02-15 00:44:01 +0530

    fix: lint

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,15 +1,13 @@
 import Footer from "./footer";
-import Code from "./code";
 import Skills from "./skills";
 import Portfolio from "./portfolio";
-import Experience from "./experience";
+// import Experience from "./experience";
 
 function Main() {
   return (
     <>
       <Portfolio />
       <Skills />
-      {/* <Code /> */}
       {/* <Experience /> */}
       <Footer />
     </>
